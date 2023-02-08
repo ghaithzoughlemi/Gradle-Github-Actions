8c56d88373e4c281a682ee7d7309d8b82715d07c
ghaithzoughlemi <ghaithzouglami@gmail.com>
2023-02-08 13:25:08 +0100

    Trigger

--- src/main/java/com/tomgregory/Application.java
+++ src/main/java/com/tomgregory/Application.java
@@ -3,5 +3,6 @@ package com.tomgregory;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Gradle GitHub actions 4tw!");
+        System.out.println("Hello, Ghaith zoughlemi");
     }
 }
